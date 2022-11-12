2f7937619e4defd1ed0adc708f57ac7d64ca41d3
n0cti1ucent <doeshe@protonmail.com>
2022-11-12 20:44:47 +0900

    replace bxslider to slick

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,37 +1,20 @@
-//var slider;
-//var sliderFlag = false;
-//var breakpoint = 768;//768px以下の場合
-//
-//function sliderSet() {
-//    var windowWidth = window.innerWidth;
-//    if (windowWidth >= breakpoint && !sliderFlag) {//768px以上は1行でスライダー表示
-//        slider = $('.slider').bxSlider({
-//        touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
-//    	mode: 'vertical',//縦スライド指定
-//    	controls: false,//前後のコントロールを表示させない。
-//    	auto: 'true',//自動的にスライド
-//    	pager: false//ページ送り無効化
-//    });
-//
-//
-//
-//    sliderFlag = true;
-//} 
-//else if (windowWidth < breakpoint && sliderFlag) {
-//    slider.destroySlider();//bxSliderのOptionであるdestroySliderを使用してスライダーの動きを除去
-//    sliderFlag = false;
-//}
-//    }
-//
-//$(window).on('load resize', function () {
-//    sliderSet();
+//bxslider
+//$('.slider').bxSlider({
+//    touchEnabled: false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
+//    mode: 'vertical',
+//    controls: false,
+//    auto: 'true',
+//    pager: false
 //});
 
-
-$('.slider').bxSlider({
-    touchEnabled: false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
-    mode: 'vertical',
-    controls: false,
-    auto: 'true',
-    pager: false
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: false,
+    speed: 1000,
+    arrows: false,
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 1,
+    SlidesToScroll: 1,
 });
