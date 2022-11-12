b079a25bae2a2ad78244ffaf28f203fda74cc9c2
n0cti1ucent <doeshe@protonmail.com>
2022-11-12 22:07:41 +0900

    replace bxslider to slick

--- assets/js/news.js
+++ assets/js/news.js
@@ -9,10 +9,8 @@
 
 $('.sticker').slick({
     autoplay: true,
-    autoplaySpeed: 2000,
     infinite: true,
     fade: false,
-    speed: 1000,
     arrows: false,
     vertical: true,
     verticalSwiping: true,
