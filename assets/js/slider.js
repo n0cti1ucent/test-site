626d68cf52a51499add98f55107fd493a09c4682
n0cti1ucent <doeshe@protonmail.com>
2022-11-13 05:34:40 +0900

    adjust recruit

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,7 @@
 $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    pauseOnFocus: false,
     slidesToShow: 3,//スライドを画面に3枚見せる
     slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
     prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
